2e3eadf4242cebe618c94d0a00feea1e1cd46b85
Darren Rector <darrenrector91@gmail.com>
2017-12-12 12:13:34 -0600

    initial commit

--- client.js
+++ client.js
@@ -1,12 +1,60 @@
-var atticus = { name: "Atticus", employeeNumber: "2405", annualSalary: "47000", reviewRating: 3 };
-var jem = { name: "Jem", employeeNumber: "62347", annualSalary: "63500", reviewRating: 4 };
-var boo = { name: "Boo", employeeNumber: "11435", annualSalary: "54000", reviewRating: 3 };
-var scout = { name: "Scout", employeeNumber: "6243", annualSalary: "74750", reviewRating: 5 };
-var robert = { name: "Robert", employeeNumber: "26835", annualSalary: "66000", reviewRating: 1 };
-var mayella = { name: "Mayella", employeeNumber: "89068", annualSalary: "35000", reviewRating: 2 };
 
+class EmployeeObject {
+
+  constructor(name, employeeNumber, annualSalary, reviewRating) {
+
+    this.name = name;
+    this.employeeNumber = employeeNumber;
+    this.annualSalary = annualSalary;
+    this.reviewRating = reviewRating;
+
+  }
+}
+
+  function bonusPercentage (person) {
+    var bonusPercent = 0;
+    if (person.reviewRating <= 2){
+      bonusPercent = 0;
+    }
+    else if(person.reviewRating == 3){
+      bonusPercent = 0.04;
+    }
+    else if(person.reviewRating == 4){
+      bonusPercent = 0.06;
+    }
+    else{
+      bonusPercent = 0.1;
+    }
+    if(person.employeeNumber.length == 4){
+      bonusPercent += 0.05;
+    }
+    if(person.annualSalary > 65000){
+      bonusPercent -= 0.01;
+    }
+    if(bonusPercent > 0.13){
+      bonusPercent = 0.13;
+    }
+    if(bonusPercent < 0){
+      bonusPercent = 0;
+    }
+    return bonusPercent;
+    }
+  var bonus = 0;
+  function totalBonus (person){
+      var bonus = person.annualSalary * bonusPercentage(person);
+      return Math.round(bonus);
+    }
+
+    function totalCompensation(person){
+    return  totalBonus(person) + parseInt(person.annualSalary);
+  }
+
+var atticus = new EmployeeObject( "Atticus", "2405","47000",3);
+var jem = new EmployeeObject("Jem", "62347","63500",4 );
+var boo = new EmployeeObject(  "Boo","11435","54000",3 );
+var scout = new EmployeeObject("Scout","6243","74750",5 );
+var robert = new EmployeeObject("Robert","26835","66000",1 );
+var mayella = new EmployeeObject("Mayella","89068","35000",2 );
 var employees = [ atticus, jem, boo, scout, robert, mayella ];
-
-// YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
-
 console.log(employees);
+console.log(bonusPercentage(robert));
